37ac2869bbbed23e17e33d662e414236eb2dc92b
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-11-10 11:09:25 -0300

    .

--- FrontEnd/src/Components/Investments/ValuesSlideBar.jsx
+++ FrontEnd/src/Components/Investments/ValuesSlideBar.jsx
@@ -1,10 +1,10 @@
 const ValuesSlideBar = () => {
     return (
-        <div class="flex flex-col items-center space-y-2 gap-2">
+        <div class="flex flex-col h-auto w-96 items-center space-y-2 gap-2">
             <h3 class="text-[#B6B6B6] text-xl">Eu tenho R$00,00</h3>
             <input
                 type="range"
-                class="w-1/2 h-1 bg-[#B6B6B6] rounded-full appearance-none outline-none accent-[#B6B6B6] 
+                class="w-full h-1 bg-[#B6B6B6] rounded-full appearance-none outline-none accent-[#B6B6B6] 
                [&::-webkit-slider-thumb]:appearance-none 
                [&::-webkit-slider-thumb]:w-4 
                [&::-webkit-slider-thumb]:h-4 
@@ -14,7 +14,7 @@ const ValuesSlideBar = () => {
             <h3 class="text-[#B6B6B6] text-xl">Aplicar durante 0 meses</h3>
             <input
                 type="range"
-                class="w-1/2 h-1 bg-[#B6B6B6] rounded-full appearance-none outline-none accent-[#B6B6B6] 
+                class="w-full h-1 bg-[#B6B6B6] rounded-full appearance-none outline-none accent-[#B6B6B6] 
                [&::-webkit-slider-thumb]:appearance-none 
                [&::-webkit-slider-thumb]:w-4 
                [&::-webkit-slider-thumb]:h-4 
--- FrontEnd/src/Views/Investments/index.jsx
+++ FrontEnd/src/Views/Investments/index.jsx
@@ -6,10 +6,10 @@ const Investments = () => {
         <div className="flex-col bg-gradient-to-b from-[#000000] to-[#222222] min-h-screen justify-center items-center">
             <Header />
             <div className='flex gap-5 justify-center mt-40'>
-                <div className='w-[580px] h-60 bg-[#0D0D0D] p-10 rounded-xl'>
+                <div className='flex justify-center items-center w-[580px] h-72 bg-[#0D0D0D] p-10 rounded-xl'>
                     <ValuesSlideBar />
                 </div>
-                <div className='w-[580px] bg-slate-600 p-10 rounded-xl'>
+                <div className='flex justify-center items-center w-[580px] h-72 bg-slate-600 p-10 rounded-xl'>
                     <ValuesSlideBar />
                 </div>
             </div>
