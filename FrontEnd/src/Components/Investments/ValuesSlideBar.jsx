ef87840586334dd1dabd0e4a99993ab9c7ad011a
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-11-10 12:34:43 -0300

    .

--- FrontEnd/src/Components/Investments/ValuesSlideBar.jsx
+++ FrontEnd/src/Components/Investments/ValuesSlideBar.jsx
@@ -1,28 +1,82 @@
+import { useState } from "react";
+
 const ValuesSlideBar = () => {
+    const [labelValueMoney, setLabelValueMoney] = useState(0);
+    const [labelValueMonth, setLabelValueMonth] = useState(1);
+
     return (
-        <div class="flex flex-col h-auto w-96 items-center space-y-2 gap-2">
-            <h3 class="text-[#B6B6B6] text-xl">Eu tenho R$00,00</h3>
-            <input
-                type="range"
-                class="w-full h-1 bg-[#B6B6B6] rounded-full appearance-none outline-none accent-[#B6B6B6] 
-               [&::-webkit-slider-thumb]:appearance-none 
-               [&::-webkit-slider-thumb]:w-4 
-               [&::-webkit-slider-thumb]:h-4 
-               [&::-webkit-slider-thumb]:rounded-full 
-               [&::-webkit-slider-thumb]:bg-amber-600"
-            />
-            <h3 class="text-[#B6B6B6] text-xl">Aplicar durante 0 meses</h3>
-            <input
-                type="range"
-                class="w-full h-1 bg-[#B6B6B6] rounded-full appearance-none outline-none accent-[#B6B6B6] 
-               [&::-webkit-slider-thumb]:appearance-none 
-               [&::-webkit-slider-thumb]:w-4 
-               [&::-webkit-slider-thumb]:h-4 
-               [&::-webkit-slider-thumb]:rounded-full 
-               [&::-webkit-slider-thumb]:bg-amber-600"
-            />
+        <div className="flex justify-center items-center gap-10">
+            <div className="flex flex-col w-[580px] h-[400px] items-center justify-center space-y-4 bg-[#0D0D0D] rounded-xl p-6">
+                <h3 className="text-[#B6B6B6] text-xl">Eu tenho R${labelValueMoney}</h3>
+                <input
+                    type="range"
+                    min="0"
+                    max="10000"
+                    step="100"
+                    value={labelValueMoney}
+                    onChange={(e) => setLabelValueMoney(Number(e.target.value))}
+                    className="w-[90%] h-1 bg-[#B6B6B6] rounded-full appearance-none outline-none accent-[#B6B6B6]
+                        [&::-webkit-slider-thumb]:appearance-none
+                        [&::-webkit-slider-thumb]:w-4
+                        [&::-webkit-slider-thumb]:h-4
+                        [&::-webkit-slider-thumb]:rounded-full
+                        [&::-webkit-slider-thumb]:bg-amber-600"
+                />
+                <h3 className="text-[#B6B6B6] text-xl">
+                    Aplicar durante {labelValueMonth} {labelValueMonth > 1 ? "meses" : "mês"}
+                </h3>
+                <input
+                    type="range"
+                    min="1"
+                    max="12"
+                    step="1"
+                    value={labelValueMonth}
+                    onChange={(e) => setLabelValueMonth(Number(e.target.value))}
+                    className="w-[90%] h-1 bg-[#B6B6B6] rounded-full appearance-none outline-none accent-[#B6B6B6]
+                        [&::-webkit-slider-thumb]:appearance-none
+                        [&::-webkit-slider-thumb]:w-4
+                        [&::-webkit-slider-thumb]:h-4
+                        [&::-webkit-slider-thumb]:rounded-full
+                        [&::-webkit-slider-thumb]:bg-amber-600"
+                />
+            </div>
+
+            <div className="flex flex-col w-[580px] h-[400px] items-center justify-center space-y-4 bg-[#0D0D0D] rounded-xl p-6">
+                <h3 className="text-[#B6B6B6] text-xl">Eu tenho R${labelValueMoney}</h3>
+                <input
+                    type="range"
+                    min="0"
+                    max="10000"
+                    step="100"
+                    value={labelValueMoney}
+                    onChange={(e) => setLabelValueMoney(Number(e.target.value))}
+                    className="w-[90%] h-1 bg-[#B6B6B6] rounded-full appearance-none outline-none accent-[#B6B6B6]
+                        [&::-webkit-slider-thumb]:appearance-none
+                        [&::-webkit-slider-thumb]:w-4
+                        [&::-webkit-slider-thumb]:h-4
+                        [&::-webkit-slider-thumb]:rounded-full
+                        [&::-webkit-slider-thumb]:bg-amber-600"
+                />
+                <h3 className="text-[#B6B6B6] text-xl">
+                    Aplicar durante {labelValueMonth} {labelValueMonth > 1 ? "meses" : "mês"}
+                </h3>
+                <input
+                    type="range"
+                    min="1"
+                    max="12"
+                    step="1"
+                    value={labelValueMonth}
+                    onChange={(e) => setLabelValueMonth(Number(e.target.value))}
+                    className="w-[90%] h-1 bg-[#B6B6B6] rounded-full appearance-none outline-none accent-[#B6B6B6]
+                        [&::-webkit-slider-thumb]:appearance-none
+                        [&::-webkit-slider-thumb]:w-4
+                        [&::-webkit-slider-thumb]:h-4
+                        [&::-webkit-slider-thumb]:rounded-full
+                        [&::-webkit-slider-thumb]:bg-amber-600"
+                />
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default ValuesSlideBar;
--- FrontEnd/src/Views/Investments/index.jsx
+++ FrontEnd/src/Views/Investments/index.jsx
@@ -1,20 +1,15 @@
-import Header from '../../Components/Header'
+import Header from '../../Components/Header';
 import ValuesSlideBar from '../../Components/Investments/ValuesSlideBar';
 
 const Investments = () => {
     return (
         <div className="flex-col bg-gradient-to-b from-[#000000] to-[#222222] min-h-screen justify-center items-center">
             <Header />
-            <div className='flex gap-5 justify-center mt-40'>
-                <div className='flex justify-center items-center w-[580px] h-72 bg-[#0D0D0D] p-10 rounded-xl'>
-                    <ValuesSlideBar />
-                </div>
-                <div className='flex justify-center items-center w-[580px] h-72 bg-slate-600 p-10 rounded-xl'>
-                    <ValuesSlideBar />
-                </div>
+            <div className="flex justify-center mt-40">
+                <ValuesSlideBar />
             </div>
         </div>
-    )
+    );
 }
 
 export default Investments;
