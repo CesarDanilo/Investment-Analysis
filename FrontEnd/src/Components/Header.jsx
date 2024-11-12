b36079a7e4c3023ce2eb4660e37d107a1f4cee84
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-11-12 14:55:40 -0400

    Update Header.jsx

--- FrontEnd/src/Components/Header.jsx
+++ FrontEnd/src/Components/Header.jsx
@@ -22,8 +22,8 @@ const Header = () => {
     return (
         <>
             <div className="w-full h-32 flex justify-center items-end">
-                <div className="w-2/5 h-14 flex justify-between bg-[#ffffff]/10 backdrop-blur-lg rounded-2xl shadow-lg shadow-amber-600 border border-white/20">
-                    {/* Icone e nome do site */}
+                <div className="w-full sm:w-3/4 md:w-2/5 h-14 flex justify-between bg-[#ffffff]/10 backdrop-blur-lg rounded-2xl shadow-lg shadow-amber-600 border border-white/20">
+                    {/* Ícone e nome do site */}
                     <div className="flex items-center">
                         <div className="w-10 h-10 ml-2 bg-amber-600 rounded-full flex items-center justify-center">
                             {/* Ícone pode ser adicionado aqui */}
@@ -31,14 +31,14 @@ const Header = () => {
                     </div>
 
                     {/* Botões */}
-                    <div className="w-4/6 h-10 mt-2 mr-2 rounded-lg flex justify-around">
+                    <div className="w-full sm:w-4/6 md:w-5/6 h-10 mt-2 mr-2 rounded-lg flex justify-around">
                         {labels.map((label, index) => (
                             <Link key={index} to={`/${label.toLowerCase()}`}>
                                 <button
                                     onClick={() => handleClick(index)}
                                     className={`px-6 py-2 font-semibold rounded-lg focus:outline-none focus:ring-2 focus:ring-opacity-50 ${activeButton === index
-                                            ? "bg-gradient-to-b from-amber-600 to-orange-700 text-white focus:ring-amber-600"
-                                            : "text-gray-400 focus:ring-amber-600"
+                                        ? "bg-gradient-to-b from-amber-600 to-orange-700 text-white focus:ring-amber-600"
+                                        : "text-gray-400 focus:ring-amber-600"
                                         }`}
                                 >
                                     {label}
@@ -50,6 +50,7 @@ const Header = () => {
                 </div>
             </div>
         </>
+
     );
 }
 
