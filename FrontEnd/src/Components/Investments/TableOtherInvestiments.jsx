66456a9cbdb8da84e7fa7a839ecf60c860defe28
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-11-12 21:34:23 -0300

    Update TableOtherInvestiments.jsx

--- FrontEnd/src/Components/Investments/TableOtherInvestiments.jsx
+++ FrontEnd/src/Components/Investments/TableOtherInvestiments.jsx
@@ -3,7 +3,7 @@ const TableOtherInvestiments = () => {
 
         <table className="table-auto bg-[#000000]/30 backdrop-blur-md  shadow-lg text-gray-200 w-full text-left rounded-lg">
             <thead className="rounded-lg">
-                <tr className="bg-[#000000]/40 rounded-lg">
+                <tr className="bg-[#000000]/20 rounded-lg">
                     <th className="px-4 py-2 font-semibold text-amber-600">Tipos de Investimentos</th>
                     <th className="px-4 py-2 font-semibold text-amber-600">Artist</th>
                     <th className="px-4 py-2 font-semibold text-amber-600">Year</th>
@@ -17,13 +17,13 @@ const TableOtherInvestiments = () => {
                 </tr>
                 <tr className="hover:bg-[#000000]/20 transition-colors rounded-lg" >
                     <td className="px-4 py-2 ">LCI (Letra de Crédito Imobiliário)</td>
-                    <td className="px-4 py-2 ">The Eagles</td>
-                    <td className="px-4 py-2 ">1972</td>
+                    <td className="px-4 py-2 "></td>
+                    <td className="px-4 py-2 "></td>
                 </tr>
                 <tr className="hover:bg-[#000000]/20 transition-colors rounded-lg">
                     <td className="px-4 py-2 ">CRI (Certificado de Recebíveis Imobiliários)</td>
-                    <td className="px-4 py-2 ">Earth, Wind, and Fire</td>
-                    <td className="px-4 py-2 ">1975</td>
+                    <td className="px-4 py-2 "></td>
+                    <td className="px-4 py-2 "></td>
                 </tr>
             </tbody>
         </table>
