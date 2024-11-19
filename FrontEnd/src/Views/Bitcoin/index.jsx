1bf4f7431a048598e24282d180f9237e02c4f1df
CSR <billypalacios616@gmail.com>
2024-11-19 14:25:32 -0300

    commit teste para comando git terminal

--- FrontEnd/src/Views/Bitcoin/index.jsx
+++ FrontEnd/src/Views/Bitcoin/index.jsx
@@ -5,6 +5,7 @@ const Bitcoin = () => {
         <div className="bg-gradient-to-b from-[#000000] to-[#222222] min-h-screen">
             <Header />
             <h1 className="text-yellow-400">Pagina de Bitcoin</h1>
+            <h2>Praticando comandos git terminal</h2>
         </div>
     )
 }
