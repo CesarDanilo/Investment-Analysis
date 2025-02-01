941c789b8152bc1303d994e7028c07f270a44ae6
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-02-01 15:46:11 -0300

    inplemntando pagina de bitcoin

--- FrontEnd/src/Views/Bitcoin/index.jsx
+++ FrontEnd/src/Views/Bitcoin/index.jsx
@@ -1,12 +1,24 @@
 import Header from '../../Components/Header'
-
+import bit from '../../../public/bit.png'
 const Bitcoin = () => {
+
+    // Link de api que traz o valor do bitcoin atual em dolar
+    // https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd
+
     return (
-        <div className="bg-gradient-to-b from-[#000000] to-[#222222] min-h-screen">
-            <Header />
-            <h1 className="text-yellow-400">Pagina de Bitcoin</h1>
-            <h2>Praticando comandos git terminal</h2>
-        </div>
+        <>
+            <div className="bg-gradient-to-b from-[#000000] to-[#222222] min-h-screen flex flex-col itens justify-center items-center">
+                <Header className="fixed top-0 left-0 w-full" />
+                <div className='mt-44 flex-col justify-center items-center flex-grow'>
+                    <div className='w-52 h-full'>
+                        <img src={bit} alt="" />
+                    </div>
+                    <div>
+                        <h1 className='text-green-500 '>$101,778.79 USD </h1>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
