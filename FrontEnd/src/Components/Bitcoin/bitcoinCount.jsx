9b6bf19ba4f4c40e6b7d42ad00d0cf2961bae1bf
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-02-04 23:34:09 -0300

    created function bitCountAPI

--- FrontEnd/src/Components/Bitcoin/bitcoinCount.jsx
+++ FrontEnd/src/Components/Bitcoin/bitcoinCount.jsx
@@ -1,3 +1,13 @@
+const api = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+
+const bitCountAPI = async () => {
+    try {
+
+    } catch (error) {
+        console.log(`Erro na hora de buscar dados da api ${error}`)
+    }
+}
+
 const BitcoinCount = () => {
     return (
         <div>
